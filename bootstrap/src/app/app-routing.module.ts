bf08bb60441ecc088cf85dbe4fe8d5eafed6d90f
yasmine_yacoub <yasmine.yacoub.3@gmail.com>
2022-09-21 16:28:54 +0100

    challenge code

--- bootstrap/src/app/app-routing.module.ts
+++ bootstrap/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ChallengeComponent } from './challenge/challenge.component';
+import { Challenge1Component } from './challenge1/challenge1.component';
 import { CssCommandComponent } from './css-command/css-command.component';
 import { WebComponent } from './web/web.component';
 
@@ -9,6 +11,10 @@ const routes: Routes = [
   { path: 'css', component: CssCommandComponent },
   { path: 'web', component: WebComponent },
   { path: 'about', component:  AboutComponent },
+  { path: 'challenge', component:  ChallengeComponent },
+  { path: 'challenge1', component:  Challenge1Component },
+  
+  
 ];
 
 @NgModule({
--- bootstrap/src/app/app.module.ts
+++ bootstrap/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CssCommandComponent } from './css-command/css-command.component';
 import { WebComponent } from './web/web.component';
 import { AboutComponent } from './about/about.component';
+import { ChallengeComponent } from './challenge/challenge.component';
+import { Challenge1Component } from './challenge1/challenge1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CssCommandComponent,
     WebComponent,
-    AboutComponent
+    AboutComponent,
+    ChallengeComponent,
+    Challenge1Component
   ],
   imports: [
     BrowserModule,
